62d48eb07e8a8007151a1ed9bd4ab38827c73a5a
Harutyunyan-Erik <erik2698@gmail.com>
2024-06-17 16:39:38 +0400

    added my-app

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-const jsContainer = document.getElementById("js");
 
-const reactContainer = document.getElementById("react");
+
+const jsContainer = document.getElementById('js');
+
+const reactContainer = document.getElementById('react');
 
 const root = ReactDOM.createRoot(reactContainer);
 
 const render = () => {
     // Core JS
-    jsContainer.innerHTML =`
+    jsContainer.innerHTML = `
         <div class="demo">
             <h2>Hello JS</h2>
-            <p>${new Date()}</p>
+            <p>${new Date()}</p> 
         </div>
     `
 
+
     // React
 
-    root.render(
+    root.render(  //methods
         React.createElement(
-            "div",
+            'div',
             null,
-            React.createElement("h2",null,"Hello React 2"),
+            React.createElement('h2', null, 'Hello React'),
             React.createElement(
-                "p",
+                'p',
                 null,
                 new Date().toString()
             )
         )
-    )
+    )    
 }
 
-setInterval(render, 1000);
 
-console.log(root);
+const obj = new Object();
+
+
+setInterval(render, 1000);
